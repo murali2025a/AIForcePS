b49c2d9ddc0f834dcaa81aa8899f30a8c82fe2ed
murali2025a <muralimohanar.vanga@hcltech.com>
2025-12-02 17:38:37 +0530

    Delete Source directory
    updated login service code added .removed existing directory

--- Source/src/models/user_model.py
+++
@@ -1,5 +0,0 @@
-class User:
-    def __init__(self, username, password, mfa_enabled=False):
-        self.username = username
-        self.password = password
-        self.mfa_enabled = mfa_enabled
